69eccc24e4c162fb87266a404329dfdce004ec5b
CeezR <cesarforthomme@gmail.com>
2024-06-29 10:25:16 +0200

    feat: project initialization

---
+++ magic-potion-shop/src/main/java/dev/cesar/magic_potion_shop/MagicPotionShopApplication.java
@@ -0,0 +1,13 @@
+package dev.cesar.magic_potion_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicPotionShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicPotionShopApplication.class, args);
+	}
+
+}
---
+++ magic-potion-shop/src/test/java/dev/cesar/magic_potion_shop/MagicPotionShopApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cesar.magic_potion_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicPotionShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
